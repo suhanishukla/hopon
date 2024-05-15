52b7eae8e3002b0034b30acd693f2089a64f4bf4
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2024-05-15 00:02:19 -0700

    added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { ReactTyped } from 'react-typed';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import logo from './images/logo.png'
 import useStyles from './styles';
+import { NavBar } from './components/NavBar/navbar.jsx';
 
 const App = () => {
     const classes = useStyles();
-
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography variant="h2" className={classes.heading}  align="left">
-                    HopOn
-                </Typography>
-                <img src={logo} className={classes.image} alt="logo" height="60" width="60"/>
+                
+                <NavBar />
             </AppBar>
         </Container>
     );
---
+++ client/src/components/NavBar/navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Box, Button } from '@material-ui/core';
+import useStyles from './styles';
+
+
+export const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar >
+      <Toolbar>
+        <IconButton edge="start" aria-label="menu">
+          {/* Your icon component */}
+        </IconButton>
+        <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+          HopOn!
+        </Typography>
+        <Box>
+          <Button color="inherit" >
+            <Typography variant="body1">
+              Find a Ride
+            </Typography>
+          </Button>
+        </Box>
+        <Box>
+          <Button color="inherit">
+            <Typography variant="body1">
+              Post a Ride
+            </Typography>
+          </Button>
+        </Box>
+        <Box>
+          <Button color="inherit">
+            <Typography variant="body1">
+              Log In
+            </Typography>
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
