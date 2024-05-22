32220519b551d0d53af91941e83e3b35bce6ec2f
sanjanarathore <sanjanar232@gmail.com>
2024-05-22 10:49:18 -0700

    updated versions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FindRide from './pages/FindRide';
 import Home from './pages/Home';
 import PostRide from './pages/PostRide';
@@ -11,13 +11,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/Find a Ride" component={FindRide} />
-        <Route path="/Post a Ride" component={PostRide} />
-        <Route path="/LogIn" component={Login} />
-        <Route path="/SignUp" component={SignUp} /> {/* Route to SignUp component */}
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/Find a Ride" element={<FindRide/>} />
+        <Route path="/Post a Ride" element={<PostRide/>} />
+        <Route path="/LogIn" element={<Login/>} />
+        <Route path="/SignUp" element={<SignUp/>} /> {/* Route to SignUp component */}
+      </Routes>
     </BrowserRouter>
   );
 }
