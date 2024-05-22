b955a86ae94194d7a20ba34801504bd8636b9f5a
Sneha Agarwal <snehaagar2023@gmail.com>
2024-05-21 22:47:03 -0700

    fix the nav bar and page structure

---
+++ client/src/pages/FindRide.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FindRide() {
+  return (
+    <div>
+      <p>Find a ride...</p>
+      <p>Use our service to find the best rides available in your area.</p>
+      <p>Search, compare, and book rides easily with just a few clicks.</p>
+      <p>Enjoy your journey with our trusted and verified drivers.</p>
+    </div>
+  );
+}
---
+++ client/src/pages/Login.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Home() {
+  return <h1>Log In Page</h1>;
+}
