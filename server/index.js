b63ae8d87045cef315e3c7e092e7c1fa0e420fe2
Rushil Shah <rushilshah2016@gmail.com>
2024-05-21 23:43:49 -0700

    add the backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express"
+import mongoose from "mongoose";
+import workoutRoutes from './routes/workouts.js';
 
-const express = require('express');
+//express App
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next)=> {
+    console.log(req.path, req.method)
+    next()
+})
+
+//react to requests
+app.use('/api/workouts',workoutRoutes)
+
+//connect to mongoDB
+mongoose.connect("mongodb+srv://thenerd127:eggertsucks@mernapp.fewuv4o.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp")
+    .then(()=> {
+        //listen from requesets
+        app.listen(4000, ()=>{
+            console.log("listening on port 4000!!")
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
