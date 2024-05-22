5f28170a158b29c81ccbaa1b8837cd3230c9c35b
sanjanarathore <sanjanar232@gmail.com>
2024-05-22 11:27:21 -0700

    fixed dependencies to work with material ui card

--- client/src/components/RideCard.js
+++ client/src/components/RideCard.js
@@ -63,12 +63,14 @@
 // }
 
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+
+import {Box, Card, CardContent, Typography} from '@material-ui/core';
 
 // const bull = (
 //   <Box
@@ -92,15 +94,15 @@ import Typography from '@mui/material/Typography';
 
 export default function RideCard() {
   return (
-    <React.Fragment>
     <Card variant="outlined">
+<React.Fragment>
     <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
         Word of the Day
         </Typography>
     </CardContent>
-</Card>
 </React.Fragment>
+</Card> 
   );
 }
 
