bbf503ab0e7d33f58dc3d320483d268d6ec04401
Rushil Shah <rushilshah2016@gmail.com>
2024-05-21 23:43:49 -0700

    add the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {useEffect, useState} from 'react'
 
 const App = () => {
+
+    const [users,setUsers] = useState(null)
+    useEffect(()=> {
+        const fetchUsers = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+            const rushilUsers = json.filter(user => user.first_name.toLowerCase() === 'rushil'); //filter for ava shah
+            if (response.ok)
+            {
+                setUsers(rushilUsers)
+            }
+        }
+        fetchUsers()
+    }, [])
+
+
     return (
         <div>
             <h1>App</h1>
+            <div className='home'>
+                {users && users.map((users) => (
+                    <p key={users._id}>{users.first_name} {users.last_name}</p>
+                ))}
+            </div>
         </div>
     );
 }
