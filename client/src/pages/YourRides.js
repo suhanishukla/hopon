38ce229b495651e454de7f637cc39248b78ef72c
Aaryan Rustagi <aaryanrustagi@Aaryans-MacBook-Pro.local>
2024-05-30 01:41:23 -0700

    added my profile dropdown with logout when logged in

---
+++ client/src/pages/YourRides.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function YourRides() {
+  return (
+    <div>
+      <h1>Your Rides</h1>
+      {/* Your rides content goes here */}
+    </div>
+  );
+}
