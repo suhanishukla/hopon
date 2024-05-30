871400b4d45d7f0990b312961c55764f93aa8931
riyagupta535 <riyagupta5@g.ucla.edu>
2024-05-30 16:32:46 -0700

    preparing ridecard frontend to be hooked up with backend

--- client/src/components/ridecard.js
+++ client/src/components/ridecard.js
@@ -1,121 +1,72 @@
-import * as React from 'react';
-import { Box, Card, CardActions, CardContent, Button, Typography} from '@material-ui/core'
-
-// from '@mui/material/Box';
-// import { Card } from '@mui/material/Card';
-// import { CardActions } from '@mui/material/CardActions';
-// import { CardContent } from '@mui/material/CardContent';
-// import { Button } from '@mui/material/Button';
-// import { Typography } from '@mui/material/Typography';
+import React from 'react';
+import { Box, Card, CardActions, CardContent, Typography, IconButton } from '@material-ui/core';
 import { IoMdPerson } from "react-icons/io";
-import { FaClock } from "react-icons/fa";
+import { FaClock, FaArrowRight, FaCalendarAlt } from "react-icons/fa";
+import RidePopup from '../components/ridepopup';
 import { RxDot } from "react-icons/rx";
-import { IoIosArrowForward } from "react-icons/io";
 
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-  </Box>
-);
-
-const items1 = [
-  { name: 'pity party on wheels', spacing: '20px', fontSize: '30px' },
-  { name: <><IoMdPerson /> 4</>, spacing: '10px', fontSize: '15px' },
-  { name: <><FaClock /> 4:30 PM</>, spacing: '15px', fontSize: '15px' }
-];
-
-export default function RideCard() {
-  const card = (
-    <React.Fragment>
-      <CardContent className="cardContent" style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          {items1.map((item, index) => (
-            <span key={index} style={{ marginRight: item.spacing, fontSize: item.fontSize }} >
-              {item.name}
-              {index !== items1.length - 1 && bull}
-            </span>
-          ))}
-          <Typography sx={{ mb: 0.5, mt: 2 }} color="#3d2814">
-            <RxDot /> UCLA
-            <br/>
-            <RxDot /> USC
-          </Typography>
-        </div>
-        <div>
-          <CardActions sx={{ justifyContent: 'flex-end', mt: 4 }}>
-            <Button style={{ fontSize: '20px', color: '#3d2814' }}>
-              <IoIosArrowForward />
-            </Button>
-          </CardActions>
-        </div>
-      </CardContent>
-    </React.Fragment>
+export default function RideCard({ ridename, startLocation, endLocation, date, time, totalPassengers, passengerList, additionalInfo }) {
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    />
   );
 
+  const items1 = [
+    { name: ridename, spacing: '20px', fontSize: '30px' },
+    { name: <><IoMdPerson /> {totalPassengers}</>, spacing: '10px', fontSize: '15px' },
+    { name: <><FaCalendarAlt /> {formatDate(date)}</>, spacing: '15px', fontSize: '15px' },
+    { name: <><FaClock /> {formatTime(time)}</>, spacing: '15px', fontSize: '15px' }
+  ];
+
+  function formatDate(dateTimeString) {
+    const dateTime = new Date(dateTimeString);
+    const formattedDate = dateTime.toLocaleDateString();
+    return formattedDate;
+  }
+
+  function formatTime(dateTimeString) {
+    const dateTime = new Date(dateTimeString);
+    const formattedTime = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return formattedTime;
+  }
+
   return (
-    <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined" sx={{ borderWidth: '2px', borderColor: '#3d2814' }}>
-        {card}
+    <Box sx={{ minWidth: 275, padding: '20px' }}>
+      <Card variant="outlined" style={{ borderWidth: '2px', borderColor: '#3d2814', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
+        <CardContent className="cardContent" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            {items1.map((item, index) => (
+              <span key={index} style={{ marginRight: item.spacing, fontSize: item.fontSize }} >
+                {item.name}
+                {index !== items1.length - 1 && bull}
+              </span>
+            ))}
+            <Typography sx={{ mb: 0.5, mt: 2 }} color="#3d2814">
+              <RxDot /> {startLocation}
+              <br />
+              <RxDot /> {endLocation}
+            </Typography>
+          </div>
+          <div>
+            <CardActions sx={{ justifyContent: 'flex-end', mt: 4 }}>
+              <IconButton>
+                <RidePopup
+                  ridename={ridename}
+                  startLocation={startLocation}
+                  endLocation={endLocation}
+                  date={date}
+                  time={time}
+                  totalPassengers={totalPassengers}
+                  passengerList={passengerList}
+                  additionalInfo={additionalInfo}
+                />
+              </IconButton>
+            </CardActions>
+          </div>
+        </CardContent>
       </Card>
     </Box>
   );
 }
-
-// import * as React from 'react';
-// // import Box from '@mui/material/Box';
-// // import Card from '@mui/material/Card';
-// // import CardActions from '@mui/material/CardActions';
-// // import CardContent from '@mui/material/CardContent';
-// // import Button from '@mui/material/Button';
-// // import Typography from '@mui/material/Typography';
-
-// import {Box, Card, CardContent, Typography} from '@material-ui/core';
-
-// // const bull = (
-// //   <Box
-// //     component="span"
-// //     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-// //   >
-// //     •
-// //   </Box>
-// // );
-
-// // const card = () => {
-// //     return(
-// //   <React.Fragment>
-// //     <CardContent>
-// //       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-// //         Word of the Day
-// //       </Typography>
-// //     </CardContent>
-// //   </React.Fragment>);
-// // };
-
-// export default function RideCard() {
-//   return (
-//     <Card variant="outlined">
-// <React.Fragment>
-//     <CardContent>
-//         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//         Word of the Day
-//         </Typography>
-//     </CardContent>
-// </React.Fragment>
-// </Card> 
-//   );
-// }
-
-
-// {/* <Card variant="outlined">
-// <React.Fragment>
-//     <CardContent>
-//         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//         Word of the Day
-//         </Typography>
-//     </CardContent>
-// </React.Fragment>
-// </Card> */}
-
