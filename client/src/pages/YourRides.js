4bc2dc12f59aa71c20d5a9173603543afbfd23e3
Aaryan Rustagi <aaryanrustagi@Aaryans-MacBook-Pro.local>
2024-06-04 21:48:38 -0700

    added section in your rides which shows rides you have joined. also added join ride feature which lets user join a ride, updates seats available, and also only shows rides that have seats open.

--- client/src/pages/YourRides.js
+++ client/src/pages/YourRides.js
@@ -1,9 +1,9 @@
-// In YourRides.js
 import React, { useEffect, useState } from 'react';
 import RideCard from '../components/RideCard';
 
 export default function YourRides() {
-  const [rides, setRides] = useState([]);
+  const [postedRides, setPostedRides] = useState([]);
+  const [joinedRides, setJoinedRides] = useState([]);
 
   useEffect(() => {
     const fetchUserRides = async () => {
@@ -21,31 +21,76 @@ export default function YourRides() {
         }
 
         const json = await response.json();
-        setRides(json);
+        setPostedRides(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const fetchJoinedRides = async () => {
+      const token = sessionStorage.getItem('token');
+
+      try {
+        const response = await fetch("http://localhost:4000/api/workouts/joinedRides", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch joined rides");
+        }
+
+        const json = await response.json();
+        setJoinedRides(json);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchUserRides();
+    fetchJoinedRides();
   }, []);
 
   return (
     <div className="background">
-      {rides.map((ride, index) => (
+      <h2>Posted Rides</h2>
+      {postedRides.map((ride, index) => (
         <RideCard 
           key={index}
+          rideId={ride._id}
           ridename={ride.rideName}
           startLocation={ride.start}
           endLocation={ride.end}
           date={ride.createdAt}
           time={ride.rideTime}
+          currentPassengers={ride.currentPassengers}
           totalPassengers={ride.passengers}
           passengerList={[]}
           additionalInfo="your mom"
         />
       ))}
-      {console.log(rides)}
+      {console.log(postedRides)}
+
+      <h2>Joined Rides</h2>
+      {joinedRides.map((ride, index) => (
+        <RideCard 
+          key={index}
+          rideId={ride._id}
+          ridename={ride.rideName}
+          startLocation={ride.start}
+          endLocation={ride.end}
+          date={ride.createdAt}
+          time={ride.rideTime}
+          currentPassengers={ride.currentPassengers}
+          totalPassengers={ride.passengers}
+          passengerList={[]}
+          additionalInfo="your mom"
+        />
+      ))}
+      {console.log(joinedRides)}
     </div>
   );
 }
+
+
